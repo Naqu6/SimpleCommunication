724bf4d2c08f4dc21083e28ae2e34693f55ddb00
Ryan Ehrlich <ryansehrlich@gmail.com>
2016-02-29 10:04:27 -0800

    Added Networking Class - Untested

--- app/src/main/java/com/com/ryanehrlich/simplecommunication/Networking.java
+++ app/src/main/java/com/com/ryanehrlich/simplecommunication/Networking.java
@@ -1,14 +1,23 @@
 package com.com.ryanehrlich.simplecommunication;
 
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+
 /**
  * Created by ryansehrlich on 2/29/16.
+ * @Author Ryan Ehrlich
  */
 public class Networking {
-    public Networking {
-        
+    private Socket socket;
+    private OutputStream out;
+
+    public Networking (String IP, int  port) throws Exception {
+        InetAddress ip = InetAddress.getByAddress(IP.getBytes());
+        this.socket = new Socket(ip, port,false);
+        this.out = socket.getOutputStream();
     }
-
-    public void main() {
-
+    public void send(String data) throws Exception {
+        out.write(data.getBytes());
     }
 }
