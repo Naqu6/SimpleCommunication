29301a61bcc2a4ca9e8fa791dc6a5935e68af530
Ryan Ehrlich <ryansehrlich@gmail.com>
2016-03-03 08:50:43 -0800

    Sockets work

--- app/src/main/java/com/com/ryanehrlich/simplecommunication/MainActivity.java
+++ app/src/main/java/com/com/ryanehrlich/simplecommunication/MainActivity.java
@@ -1,34 +1,66 @@
 package com.com.ryanehrlich.simplecommunication;
 
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    private final String IP = "10.25.1.242";
-    private final int PORT = 25546;
+    private String IP = "74.125.28.26";
+    private final int PORT = 25;
 
     protected Networking connection;
     protected EditText text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+
+        StrictMode.setThreadPolicy(policy);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        this.connection = new Networking(IP, PORT);
 
         Button send = (Button) findViewById(R.id.send);
         this.text = (EditText) findViewById(R.id.data);
 
-        send.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                connection.send(text.getText().toString());
-            }
-        });
+        send.setVisibility(View.INVISIBLE);
+        send.setClickable(false);
+
+        boolean passed = true;
+
+        try {
+            this.connection = new Networking(IP, PORT);
+        } catch (Exception e){
+            this.connection = null;
+            passed = false;
+            e.printStackTrace();
+        }
+        if (passed) {
+            send.setVisibility(View.VISIBLE);
+            send.setClickable(true);
+
+            send.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    connection.send(text.getText().toString());
+                    try {
+                        Log.d("Recved Data", connection.recive());
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        } else {
+            TextView msg = (TextView) findViewById(R.id.msg);
+            msg.setText("There was an error while creating the socket");
+        }
     }
 }
--- app/src/main/java/com/com/ryanehrlich/simplecommunication/Networking.java
+++ app/src/main/java/com/com/ryanehrlich/simplecommunication/Networking.java
@@ -1,5 +1,8 @@
 package com.com.ryanehrlich.simplecommunication;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
@@ -9,24 +12,28 @@ import java.net.Socket;
  * @Author Ryan Ehrlich
  */
 public class Networking {
-    private Socket socket;
-    private OutputStream out;
+    final int BUFFER_SIZE = 1024;
 
-    public Networking (String IP, int  port)  {
-        try {
-            InetAddress ip = InetAddress.getByAddress(IP.getBytes());
-            this.socket = new Socket(ip, port);
-            this.out = socket.getOutputStream();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    private Socket socket;
+
+    private OutputStream out;
+    private BufferedReader in;
+
+    public Networking (String IP, int  port) throws Exception {
+        this.socket = new Socket(IP, port);
+        this.out = socket.getOutputStream();
+        this.in = new BufferedReader(new InputStreamReader(socket.getInputStream()), 8 * 1024);
 
     }
     public void send(String data) {
         try {
             out.write(data.getBytes());
+            System.out.println("Sent Data");
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+    public String recive() throws Exception{
+            return in.readLine();
     }
 }
